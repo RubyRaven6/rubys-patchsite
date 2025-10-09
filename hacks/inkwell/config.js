c3fc25185b28fcea16cb6858dd7ea14cfdbb40dd
HashtagMarky <143505183+HashtagMarky@users.noreply.github.com>
2025-10-09 16:41:59 +0100

    Refactor Inkwell

---
+++ hacks/inkwell/config.js
@@ -0,0 +1,11 @@
+window.TARP_CONFIG = {
+  title: "Inkwell",
+  base: "Pokémon Emerald (USA, Europe)",
+
+  externalLinks: {
+    discord: "",
+    github: "https://github.com/DizzyEggg/pokeemerald/tree/tarc2",
+    pokécommunity: "",
+    reddit: ""
+  }
+};
