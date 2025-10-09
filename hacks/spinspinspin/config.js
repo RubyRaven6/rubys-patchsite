83f8f1b98fabb4eb0a6dde014f8eee0d6442c347
HashtagMarky <143505183+HashtagMarky@users.noreply.github.com>
2025-10-09 16:54:50 +0100

    Refactor SpinSpinSpin

---
+++ hacks/spinspinspin/config.js
@@ -0,0 +1,11 @@
+window.TARP_CONFIG = {
+  title: "Pokémon SpinSpinSpin",
+  base: "Pokémon Emerald (USA, Europe)",
+
+  externalLinks: {
+    discord: "",
+    github: "https://github.com/doodles-hearth/pokemon-hearth/tree/hacking-comp",
+    pokécommunity: "",
+    reddit: ""
+  }
+};
