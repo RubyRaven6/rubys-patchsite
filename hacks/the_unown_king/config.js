9549181181786e40335f72724197fc6037da5ff0
HashtagMarky <143505183+HashtagMarky@users.noreply.github.com>
2025-10-09 17:14:54 +0100

    Refactor The Unown King

---
+++ hacks/the_unown_king/config.js
@@ -0,0 +1,11 @@
+window.TARP_CONFIG = {
+  title: "Pokémon The Unown King",
+  base: "Pokémon Emerald (USA, Europe)",
+
+  externalLinks: {
+    discord: "",
+    github: "https://github.com/green-tee/pokeemerald-expansion-unown-king",
+    pokécommunity: "",
+    reddit: ""
+  }
+};
